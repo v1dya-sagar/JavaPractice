4901c9cc0a783b2d95e3dffe899ddea913660dd2
v1dya-sagar <vidyasagar08123@gmail.com>
2024-02-23 22:00:22 +0530

    Edit existing Code

--- com/vsk/practice/exercise/FindTheElement.java
+++ com/vsk/practice/exercise/FindTheElement.java
@@ -3,6 +3,8 @@ package com.vsk.practice.exercise;
 import java.util.*;
 public class FindTheElement {
     public static void main(String[] args) {
+
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
