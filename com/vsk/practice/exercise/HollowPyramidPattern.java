0edde53ded37ddb505c2975996679dbab227f5d5
v1dya-sagar <vidyasagar08123@gmail.com>
2024-03-02 05:22:44 +0530

    HollowPyramidPattern

---
+++ com/vsk/practice/exercise/HollowPyramidPattern.java
@@ -0,0 +1,64 @@
+package com.vsk.practice.exercise;
+
+public class HollowPyramidPattern {
+        public static void main(String[] args) {
+            int n = 5;
+            int middle_spaces = 1;
+            for(int i = 1; i <= n; i++){
+                int spaces = n - i;
+                while(spaces != 0){
+                    System.out.print(" ");
+                    spaces--;
+                }
+
+                if(i == 1){
+                    System.out.print("*");
+                } else {
+
+                    if(i == n){
+                        for(int k = 1; k <= 2*n - 1; k++){
+                            System.out.print("*");
+                        }
+                    }else{
+                        System.out.print("*");
+                        for(int a = 1; a <= middle_spaces; a++){
+                            System.out.print(" ");
+                        }
+                        System.out.print("*");
+                        middle_spaces+=2;
+                    }
+                }
+                System.out.println();
+            }
+        }
+}
+
+//class HelloWorld {
+//    public static void main(String[] args) {
+//        int n = 5;
+//        for(int i = 1; i <= n - 1; i++){
+//            int spaces = n - i;
+//            int j = 0;
+//            while(spaces != 0){
+//                System.out.print(" ");
+//                spaces--;
+//                j++;
+//            }
+//
+//            for(; j <= n + i - 1;j++){
+//                // System.out.print("*");
+//                if(j == n - i || j == n + i - 2){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//
+//            System.out.println();
+//        }
+//        for(int k = 1; k <= 2*n - 1; k++){
+//            System.out.print("*");
+//        }
+//    }
+//}
+
