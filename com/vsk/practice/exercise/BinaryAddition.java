e77518049a11b46e27571046bb4c7bc3d5b53cb9
v1dya-sagar <vidyasagar08123@gmail.com>
2024-04-06 23:55:01 +0530

    changes

--- com/vsk/practice/exercise/BinaryAddition.java
+++ com/vsk/practice/exercise/BinaryAddition.java
@@ -1,6 +1,7 @@
 package com.vsk.practice.exercise;
 
 public class BinaryAddition {
+
     public static void main(String[] args) {
         int x = 0011;
         String result = Integer.toBinaryString(x);
