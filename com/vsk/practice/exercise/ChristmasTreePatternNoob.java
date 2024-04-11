1c26769483599787bbe6290e12bf435a1e741a48
v1dya-sagar <vidyasagar08123@gmail.com>
2024-04-11 22:55:29 +0530

    Completed Christmas Tree maybe full

--- com/vsk/practice/exercise/ChristmasTreePatternNoob.java
+++ com/vsk/practice/exercise/ChristmasTreePatternNoob.java
@@ -3,7 +3,7 @@ package com.vsk.practice.exercise;
 public class ChristmasTreePatternNoob {
 
         public static void main(String[] args) {
-            int height = 5; // Height of the tree
+            int height = 5;
             printChristmasTree(height);
         }
 
@@ -20,7 +20,7 @@ public class ChristmasTreePatternNoob {
                 System.out.println();
             }
 
-
+            // Print the tree trunk
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < height - 1; j++) {
                     System.out.print(" ");
