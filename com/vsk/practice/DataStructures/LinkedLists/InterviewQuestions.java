867e2b0632f38a7319904e7a74f74661ac542ad8
v1dya-sagar <vidyasagar08123@gmail.com>
2024-06-30 22:45:08 +0530

    30-06-2024

--- com/vsk/practice/DataStructures/LinkedLists/InterviewQuestions.java
+++ com/vsk/practice/DataStructures/LinkedLists/InterviewQuestions.java
@@ -116,7 +116,7 @@ public class InterviewQuestions {
 
 
 
-    // https://leetcode.com/problems/reverse-linked-list/submissions/
+    // https://leetcode.com/problems/reverse-linked-list
     // google, apple, amazon, microsoft
     public ListNode reverseList(ListNode head) {
         if (head == null) {
@@ -176,54 +176,7 @@ public class InterviewQuestions {
     }
 
 
-    // google, amazon, facebook, microsoft: https://leetcode.com/problems/reverse-nodes-in-k-group/
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (k <= 1 || head == null) {
-            return head;
-        }
 
-        ListNode current = head;
-        ListNode prev = null;
-
-        int length = getLength(head);
-        int count = length / k;
-        while (count > 0) {
-            ListNode last = prev;
-            ListNode newEnd = current;
-
-            ListNode next = current.next;
-            for (int i = 0; current != null && i < k; i++) {
-                current.next = prev;
-                prev = current;
-                current = next;
-                if (next != null) {
-                    next = next.next;
-                }
-            }
-
-            if (last != null) {
-                last.next = prev;
-            } else {
-                head = prev;
-            }
-
-            newEnd.next = current;
-
-            prev = newEnd;
-            count--;
-        }
-        return head;
-    }
-
-    public int getLength(ListNode head) {
-        ListNode node = head;
-        int length = 0;
-        while (node != null) {
-            length++;
-            node = node.next;
-        }
-        return length;
-    }
 
     // https://www.geeksforgeeks.org/reverse-alternate-k-nodes-in-a-singly-linked-list/
     public ListNode reverseAlternateKGroup(ListNode head, int k) {
@@ -286,8 +239,54 @@ public class InterviewQuestions {
         reverseList(rereverseHead);
 
         return head == null || headSecond == null;
+    }// google, amazon, facebook, microsoft: https://leetcode.com/problems/reverse-nodes-in-k-group/
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k <= 1 || head == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        ListNode prev = null;
+
+        int length = getLength(head);
+        int count = length / k;
+        while (count > 0) {
+            ListNode last = prev;
+            ListNode newEnd = current;
+
+            ListNode next = current.next;
+            for (int i = 0; current != null && i < k; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+
+            prev = newEnd;
+            count--;
+        }
+        return head;
     }
 
+    public int getLength(ListNode head) {
+        ListNode node = head;
+        int length = 0;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
     // https://leetcode.com/problems/reorder-list/
 // Google, Facebook
     public void reorderList(ListNode head) {
