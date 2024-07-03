9a56027f4f7a9957d4746303ae1256a72954268c
v1dya-sagar <vidyasagar08123@gmail.com>
2024-07-03 22:18:58 +0530

    03-07-2024

--- com/vsk/practice/DataStructures/Stacks/Problems/MinStack.java
+++ com/vsk/practice/DataStructures/Stacks/Problems/MinStack.java
@@ -1,2 +1,41 @@
-package com.vsk.practice.DataStructures.Stacks.Problems;public class MinStack {
+package com.vsk.practice.DataStructures.Stacks.Problems;
+
+import java.util.Stack;
+
+public class MinStack {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top());    // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+        // only push the old minimum value when the current
+        // minimum value changes after pushing the new value x
+        if(x <= min){
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value,
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
 }
