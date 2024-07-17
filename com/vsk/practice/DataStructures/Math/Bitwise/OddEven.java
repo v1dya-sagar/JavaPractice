b9fb3b314c8c733c733ca9b238e11b501643bb40
v1dya-sagar <vidyasagar08123@gmail.com>
2024-07-17 11:26:58 +0530

    17-07-2024 math

---
+++ com/vsk/practice/DataStructures/Math/Bitwise/OddEven.java
@@ -0,0 +1,13 @@
+package com.vsk.practice.DataStructures.Math.Bitwise;
+
+public class OddEven {
+
+    public static void main(String[] args) {
+	    int n = 68;
+        System.out.println(isOdd(n));
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1) == 1;
+    }
+}
